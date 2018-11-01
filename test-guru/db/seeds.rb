42098447d233b193e52614a8fbf03fea0405e6a7
hookbil <fyshakov@gmail.com>
2018-11-01 23:20:01 +0300

    started to write seeds.rb

--- test-guru/db/seeds.rb
+++ test-guru/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create!([{title: "Bash"}, {title: "PostgreSQL"}, {title: "PHP"}])
+tests = Test.create!([
+	{title: "Navigation in bash", categories[0].id},
+	{title: "Writing scripts on bash", categories[0].id, level: 1},
+	{title: "SQL basics", categories[1].id},
+	{title: "PHP basics", categories[2].id}
+])
+questions = Question.create!([
+	{body: "What is the bash?", test_id: tests[0].id},
+	{body: "How to see the files in current dir?", test_id: tests[0].id},
+	{body: "How to give rights for execute the file?", test_id: tests[1].id},
+	{body: "How to assign a variable? ", test_id: tests[1].id}, 
+	{body: "What operator you should use for showing data from table?", test_id: tests[2].id},
+	{body: "How to call variable?", test_id: tests[2].id}
+])
+Answer.create!([
+	{body: ""}
+
+])
