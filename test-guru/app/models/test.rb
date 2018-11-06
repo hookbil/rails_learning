47797a4ff8ec74fecff269d9bbf9e94a74a3f912
Evgeniy Fateev <spaceflow@gmail.com>
2018-11-06 22:41:46 +0300

    Update test-guru/app/models/test.rb
    Co-Authored-By: hookbil <41017854+hookbil@users.noreply.github.com>

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
   def self.titles_by_category(category_title)
     joins("INNER JOIN categories ON tests.category_id = categories.id")
-      .where("categories.title = ?", category_title).order(:title).pluck(:title)
+      .where(categories: { title: category_title }).order(:title).pluck(:title)
   end
 end
