0ead2010cf8cc834f50074c1b30cd54943b9a3c3
Федор Ушаков <hookbil@smotreshka.tv>
2019-01-20 13:29:28 +0300

    some minor fixes

--- test-guru/app/controllers/tests_controller.rb
+++ test-guru/app/controllers/tests_controller.rb
@@ -7,28 +7,23 @@ class TestsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    # result = ["Class : #{params.class}", "Parameters: #{params.inspect}"]
-    # render plain: result.join("\n")
     @tests = Test.all
   end
 
   def show
-    # render inline: '<%= @test.title %>'
     @test = Test.find(params[:id])
   end
 
   def create
     @test = Test.new(test_params)
-    
+
     if @test.save
       redirect_to @test
     else
       render :new
     end
-    #return render plain: test.inspect if test.save
-    #render plain: "Ошибка"
   end
-  
+
   def new
     @test = Test.new
   end
@@ -54,12 +49,12 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
-	private
+  private
 
-	def test_params
-		params.require(:test).permit(:title, :level, :category_id, :author_id)
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
-  
+
   def find_test
     @test = Test.find(params[:id])
   end
@@ -72,7 +67,7 @@ class TestsController < ApplicationController
     start = Time.now
     yield
     finish = Time.now - start
-    
+
     logger.info("Execution time: #{finish * 1000}ms")
   end
 
--- test-guru/config/routes.rb
+++ test-guru/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tests , shallow: true do
-    resources :questions
+  resources :tests do
+    resources :questions, shallow: true
   end
 end
